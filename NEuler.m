a791663c54dc496f5a4eadf5d59fc1e1f2c0deaf
MrAbigor111 <igorccarvalheira111@gmail.com>
2025-04-05 18:34:14 +0100

    Método de Adams Bashworth e comentários

--- NEuler.m
+++ NEuler.m
@@ -1,23 +1,22 @@
-function [t,y] = NEuler(f,a,b,n,y0)
-%NEULER Método de Euler para resolução numérica de EDO/PVI
-%   y'=f(t,y), t=[a,b], y(a)=y0
-%   y(i+1)=y(i)+hf(t(i),y(i)), i=0,1,2,...,n
+% ---> NEULER Método de Euler para resolução numérica de EDO/PVI
 %INPUT:
-%   f - função da EDO y'=f(t,y)
-%   [a,b] - intervalo de valores da variável independente t
-%   n - núnmero de subintervalos ou iterações do método
-%   y0 - aproximação inicial y(a)=y0
+% f - função da EDO y'=f(t,y)
+% [a,b] - intervalo de valores da variável independente t
+% n - número de subintervalos ou iterações do método
+% y0 - aproximação inicial y(a)=y0
 %OUTPUT:
-%   t - vetor do intervalo [a,b] discretizado 
-%   y - vetor das soluções aproximadas do PVI em cada um dos t(i)
-%
-%   11/03/2025  Arménio Correia  armenioc@isec.pt
-
-h = (b-a)/n;
-t(1) = a;
-y(1) = y0;
-for i = 1:n
-    y(i+1) = y(i)+h*f(t(i),y(i));
-    t(i+1) = t(i)+h;
-end
-end
+% t - vetor do intervalo [a,b] discretizado
+% y - vetor das soluções aproximadas do PVI em cada um dos t(i)
+%     com t(i+1)=t(i)+h;
+%AUTORES:
+% Igor Carvalheira a2024128677@isec.pt
+% Lucas Pantarotto a2024143625@isec.pt
+function [t,y] = NEuler(f,a,b,n,y0)
+    h=(b-a)/n;                         %Calcula o passo h com base no intervalo e no número de subintervalos
+    t(1)=a;                            %Define o primeiro valor de t como o início do intervalo
+    y(1)=y0;                           %Define a condição inicial y(a)=y0
+    for i=1:n
+        y(i+1)=y(i)+h*f(t(i),y(i));    %Aplica a fórmula de Euler para calcular y(i+1)
+        t(i+1)=t(i)+h;                 %Atualiza o valor de t para o próximo passo
+    end
+    end
--- NEulerM.m
+++ NEulerM.m
@@ -1,24 +1,23 @@
-function [t,y] = NEulerM(f,a,b,n,y0)
-%NEULER Método de Euler para resolução numérica de EDO/PVI
-%   y'=f(t,y), t=[a,b], y(a)=y0
-%   y(i+1)=y(i)+hf(t(i),y(i)), i=0,1,2,...,n
+% --> NEULER Método de Euler Melhorado para resolução numérica de EDO/PVI
 %INPUT:
 %   f - função da EDO y'=f(t,y)
 %   [a,b] - intervalo de valores da variável independente t
-%   n - núnmero de subintervalos ou iterações do método
+%   n - número de subintervalos ou iterações do método
 %   y0 - aproximação inicial y(a)=y0
 %OUTPUT:
-%   t - vetor do intervalo [a,b] discretizado 
+%   t - vetor do intervalo [a,b] discretizado
 %   y - vetor das soluções aproximadas do PVI em cada um dos t(i)
 %   t(i+1) = t(i)+h;
-%   11/03/2025  Arménio Correia  armenioc@isec.pt
-%   20/03/2025  Igor Carvalheira a2024128677@isec.pt
+%AUTORES
+%  Igor Carvalheira    a2024128677@isec.pt
+%  Lucas Pantarotto    a2024143625@isec.pt
+function [t,y] = NEulerM(f,a,b,n,y0)
 
-h = (b-a)/n;
-t = a:h:b;
-y = zeros(1,4);
-y(1) = y0;
-for i = 1:n
-    y(i+1) = y(i)+h*f(t(i),y(i));
-end
-end
+    h = (b-a)/n;            % Calcula o tamanho do passo (h) com base no intervalo [a,b] e número de subintervalos n
+    t = a:h:b;              % Cria o vetor t com os pontos discretizados do intervalo [a,b]
+    y = zeros(1,4);         % Inicializa o vetor y com 4 elementos (devia ser n+1 para ser geral)
+    y(1) = y0;              % Define a condição inicial: y(a) = y0
+    for i = 1:n
+        y(i+1) = y(i)+h*f(t(i),y(i));  % Aplica o método de Euler: aproxima o valor de y(i+1) com base na inclinação local
+    end
+    end
