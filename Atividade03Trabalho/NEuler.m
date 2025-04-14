b3f02623e590d5964e18ec9a77516baf9fa0a157
MrAbigor111 <igorccarvalheira111@gmail.com>
2025-04-14 09:58:47 +0100

    App falta só os autores e modificação nas funções

--- Atividade03Trabalho/NEuler.m
+++ Atividade03Trabalho/NEuler.m
@@ -13,11 +13,11 @@
 % Lucas Pantarotto a2024143625@isec.pt
 % Rafael Carvalho a2024143302@isec.pt
 function [t,y] = NEuler(f,a,b,n,y0)
-    h=(b-a)/n;                         %Calcula o passo h com base no intervalo e no número de subintervalos
-    t(1)=a;                            %Define o primeiro valor de t como o início do intervalo
-    y(1)=y0;                           %Define a condição inicial y(a)=y0
-    for i=1:n
-        y(i+1)=y(i)+h*f(t(i),y(i));    %Aplica a fórmula de Euler para calcular y(i+1)
-        t(i+1)=t(i)+h;                 %Atualiza o valor de t para o próximo passo
+    h = (b-a)/n;            % Calcula o tamanho do passo (h) com base no intervalo [a,b] e número de subintervalos n
+    t = a:h:b;              % Cria o vetor t com os pontos discretizados do intervalo [a,b]
+    y = zeros(1,n+1);        % Inicializa o vetor y com n elementos (devia ser n+1 para ser geral)
+    y(1) = y0;              % Define a condição inicial: y(a) = y0
+    for i = 1:n
+        y(i+1) = y(i)+h*f(t(i),y(i));  % Aplica o método de Euler: aproxima o valor de y(i+1) com base na inclinação local
     end
     end
--- Atividade03Trabalho/NEulerM.m
+++ Atividade03Trabalho/NEulerM.m
@@ -1,4 +1,4 @@
-% --> NEULER Método de Euler Melhorado para resolução numérica de EDO/PVI
+% --> NEULER Método de Euler Melhorado (Heun) para resolução numérica de EDO/PVI
 %INPUT:
 %   f - função da EDO y'=f(t,y)
 %   [a,b] - intervalo de valores da variável independente t
@@ -12,13 +12,16 @@
 % Igor Carvalheira a2024128677@isec.pt
 % Lucas Pantarotto a2024143625@isec.pt
 % Rafael Carvalho a2024143302@isec.pt
-function [t,y] = NEulerM(f,a,b,n,y0)
+function [t, y] = NEulerM(f, a, b, n, y0)
+    h = (b - a) / n;         % Passo
+    t = a:h:b;               % Vetor de tempo
+    y = zeros(1, n+1);       % Inicializa vetor das soluções
+    y(1) = y0;               % Condição inicial
 
-    h = (b-a)/n;            % Calcula o tamanho do passo (h) com base no intervalo [a,b] e número de subintervalos n
-    t = a:h:b;              % Cria o vetor t com os pontos discretizados do intervalo [a,b]
-    y = zeros(1,n+1);        % Inicializa o vetor y com 4 elementos (devia ser n+1 para ser geral)
-    y(1) = y0;              % Define a condição inicial: y(a) = y0
     for i = 1:n
-        y(i+1) = y(i)+h*f(t(i),y(i));  % Aplica o método de Euler: aproxima o valor de y(i+1) com base na inclinação local
+        k1 = f(t(i), y(i));                          % Pendente no início
+        y_pred = y(i) + h * k1;                      % Previsão com Euler
+        k2 = f(t(i+1), y_pred);                      % Pendente na previsão
+        y(i+1) = y(i) + (h/2) * (k1 + k2);            % Correção (média das pendentes)
     end
-    end
+end
