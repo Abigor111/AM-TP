ef3212caf757c0b0d026449bbbda96311938580f
MrAbigor111 <igorccarvalheira111@gmail.com>
2025-05-15 18:14:25 +0100

    Update ao switch
    O Switch não pode ser usado, devido a haverem 3 tabs, logo o switch vai ser usado na 3ª tab para alterar entre polares e cartesianas

--- Atividade05Trabalho/NDerivacaoDFR.m
+++ Atividade05Trabalho/NDerivacaoDFR.m
@@ -1,13 +1,29 @@
-function [x,y,dydx] = NDerivacaoDFR(~, f,a,b,h, y)
-    x = a:h:b;
-    n = length(x);
-    if nargin == 4
-        y = f(x);
+%---> NDerivacaoDFR Derivação numérica usando diferenças finitas regressivas
+%
+%INPUT:
+% f  - função a derivar (ou vetor de valores se y for omitido)
+% [a,b] - intervalo onde se pretende calcular a derivada
+% h  - passo de discretização
+% y  - (opcional) vetor de valores já calculados da função f(x)
+%
+%OUTPUT:
+% x  - vetor de pontos no intervalo [a,b] com passo h
+% y  - vetor dos valores de f(x) nos pontos x(i)
+% dydx - vetor das derivadas aproximadas em cada x(i)
+%
+%AUTORES:
+% Igor Carvalheira    a2024128677@isec.pt
+% Lucas Pantarotto    a2024143625@isec.pt
+% Rafael Carvalho     a2024143302@isec.pt
+function [x,y,dydx] = NDerivacaoDFR(~,f,a,b,h,y)
+    x = a:h:b;              % Cria vetor de pontos entre a e b com passo h
+    n = length(x);          % Número total de pontos
+    if nargin == 5
+        y = f(x);           % Calcula os valores da função se y não for dado
     end
-    dydx = zeros(1,n);
-    dydx(1) = (y(2) - y(1)) / h;           % Ainda é progressiva no primeiro ponto!
-    for i = 2:n
-        dydx(i) = (y(i) - y(i-1)) / h;     % Diferença regressiva
+    dydx = zeros(1,n);      % Inicializa o vetor da derivada com zeros
+    dydx(1) = (y(2)-y(1))/h; % Estimativa inicial usando diferença progressiva
+    for k = 2:n
+        dydx(k) = (y(k)-y(k-1))/h; % Diferença regressiva para os restantes pontos
     end
-    dydx(n) = (y(n)-y(n-1))/h;
 end
